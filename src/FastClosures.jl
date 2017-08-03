be69d25012796ee8980e125111ad842025ebfcf7
Chris Foster <chris42f@gmail.com>
2017-08-03 19:55:08 +1000

    Improved documentation

--- src/FastClosures.jl
+++ src/FastClosures.jl
@@ -7,9 +7,36 @@ using Base.Meta
 
 export @closure
 
-macro closure(ex_orig)
+"""
+    @closure closure_expression
+
+Wrap the closure definition `closure_expression` in a let block to encourage
+the julia compiler to generate improved type information.  For example:
+
+```julia
+callfunc(f) = f()
+
+function foo(n)
+   for i=1:n
+       if i >= n
+           # Unlikely event - should be fast.  However, capture of `i` inside
+           # the closure confuses the julia-0.6 compiler and causes it to box
+           # the variable `i`, leading to a 100x performance hit if you remove
+           # the `@closure`.
+           callfunc(@closure ()->println("Hello \$i"))
+       end
+   end
+end
+```
+
+There's nothing nice about this - it's a heuristic workaround for some
+inefficiencies in the type information inferred by the julia 0.6 compiler.
+However, it can result in large speedups in many cases, without the need to
+restructure the code to avoid the closure.
+"""
+macro closure(closure_expression)
     ex = Compat.macros_have_sourceloc ?
-         macroexpand(__module__, ex_orig) : macroexpand(ex_orig)
+         macroexpand(__module__, closure_expression) : macroexpand(closure_expression)
     if isexpr(ex, :(->))
         args1 = ex.args[1]
         if isa(args1, Symbol)
@@ -31,7 +58,7 @@ macro closure(ex_orig)
     find_var_uses!(captured_vars, bound_vars, ex.args[2])
     quote
         let $([:($(esc(v))=$(esc(v))) for v in captured_vars]...)
-            $(esc(ex_orig))
+            $(esc(closure_expression))
         end
     end
 end
